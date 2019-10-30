fd44c3134950b4af16a0acfc21a506775c8d9fc0
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-30 17:28:13 +0000

    db created but Deprecation Warning

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,16 @@
 require 'bundler/setup'
-Bundler.require
+Bundler.require(:default, ENV['SINATRA_ENV'])
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
-ENV['SINATRA_ENV'] ||= "development"
+configure :development do
+  set :database, 'sqlite3:db/databse.db'
+end
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
-)
+# DEPRECATION WARNING: Setting `ActiveRecord::Base.configurations` with `[]=` is deprecated. Use `ActiveRecord::Base.configurations=` directly to set t
+# he configurations instead. (called from block in <top (required)> at /home/mystical-generator-0278/temporary/To-Do-List/config/environment.rb:9)
+# Created database 'db/databse.db'
 
 require_all './app'
